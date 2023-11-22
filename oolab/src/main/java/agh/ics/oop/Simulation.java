63dbfa4717e9bb6710afbcee78338a2c43dea817
Deaponn <bartek.sajo@interia.pl>
2023-11-22 21:28:54 +0100

    Add RectangularMap and update code to work with it

--- oolab/src/main/java/agh/ics/oop/Simulation.java
+++ oolab/src/main/java/agh/ics/oop/Simulation.java
@@ -3,6 +3,7 @@ package agh.ics.oop;
 import agh.ics.oop.model.Animal;
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.WorldMap;
 
 import java.util.List;
 import java.util.LinkedList;
@@ -11,8 +12,10 @@ import java.util.ListIterator;
 public class Simulation {
     private final List<Animal> animals = new LinkedList<>();
     private final List<MoveDirection> moves;
+    private final WorldMap animalsMap;
 
-    public Simulation(List<Vector2d> positions, List<MoveDirection> moves) {
+    public Simulation(WorldMap animalsMap, List<Vector2d> positions, List<MoveDirection> moves) {
+        this.animalsMap = animalsMap;
         this.moves = moves;
         for (int i = 0; i < positions.size(); i++) {
             animals.add(new Animal(positions.get(i)));
---
+++ oolab/src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -0,0 +1,48 @@
+package agh.ics.oop.model;
+
+import agh.ics.oop.model.util.MapVisualizer;
+
+import java.util.HashMap;
+import java.util.Map;
+public class RectangularMap implements WorldMap {
+    Map<Vector2d, Animal> animals = new HashMap<>();
+    private final Vector2d mapStart = new Vector2d(0, 0);
+    private final Vector2d mapEnd;
+    public RectangularMap(int width, int height) {
+        mapEnd = new Vector2d(width - 1, height - 1).add(mapStart);
+    }
+
+    public boolean place(Animal animal) {
+        if (isOccupied(animal.getPosition())) {
+            return false;
+        }
+        animals.put(animal.getPosition(), animal);
+        return true;
+    }
+
+    public void move(Animal animal, MoveDirection direction){
+        Vector2d animalPosition = animal.getPosition();
+        animal.move(direction, this);
+        if (!animal.isAt(animalPosition)) {
+            animals.remove(animalPosition);
+            animals.put(animal.getPosition(), animal);
+        }
+    }
+
+    public boolean canMoveTo(Vector2d position) {
+        return position.follows(mapStart) && position.precedes(mapEnd) && !animals.containsKey(position);
+    }
+
+    public boolean isOccupied(Vector2d position){
+        return animals.containsKey(position);
+    }
+
+    public Animal objectAt(Vector2d position) {
+        return animals.get(position);
+    }
+
+    @Override
+    public String toString() {
+        return new MapVisualizer(this).draw(mapStart, mapEnd);
+    }
+}
